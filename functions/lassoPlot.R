64bd876873f3b5032e4c8e75485e5cae87268f52
TimothyKBook <TimothyKBook@gmail.com>
2017-06-21 10:16:38 -0400

    Cleaned plot, initiated shiny ui, updated readme

--- functions/lassoPlot.R
+++ functions/lassoPlot.R
@@ -1,30 +1,9 @@
-library(ggplot2)
-library(mvtnorm)
-library(lattice)
-library(MASS)
-library(glmnet)
-
-set.seed(123)
-n <- 100
-x1 <- runif(n)
-x2 <- runif(n)
-
-y <- 2 * x1 + 7 * x2 + rnorm(n, 0, 5)
-
-ols <- lm(y ~ x1 + x2)
-b_ols_x <- coef(ols)[2]
-b_ols_y <- coef(ols)[3]
-df_ols <- data.frame(bx = b_ols_x, by = b_ols_y)
-
-lambda_seq <- seq(0.1, 3, 0.1)
-lasso <- glmnet(cbind(x1, x2), y, lambda = lambda_seq)
-
 plotLasso <- function(lambda, 
                       lasso = lasso, 
                       df_ols = df_ols, 
                       lambda_seq = lambda_seq) {
   
-  slope_ind <- which(abs(lambda_seq - lambda) < 1e-6)
+  slope_ind <- length(lambda_seq) - which(abs(lambda_seq - lambda) < 1e-6) + 1
   b_lasso <- coef(lasso)[2:3, slope_ind]
   t_tuning <- sum(abs(b_lasso))
   
@@ -35,11 +14,10 @@ plotLasso <- function(lambda,
   lasso_plot <- ggplot(df_ols) + 
     geom_point(aes(x = bx, y = by)) +
     geom_polygon(data = df_poly, aes(x, y), 
-                 fill = '#52B6E8', color = 'black', alpha = 1/2, size = 1) +
+                 fill = '#52B6E9', color = 'black', alpha = 1/2, size = 1) +
     geom_point(data = df_lasso, aes(x = bx, y = by), size = 3, color = 'red') +
     theme_bw() +
-    xlim(-8, 8) +
-    ylim(-8, 8) +
+    coord_map(xlim = c(-9, 9), ylim = c(-9, 9)) +
     geom_hline(yintercept = 0) +
     geom_vline(xintercept = 0) +
     labs(x = '', y = '')
@@ -47,5 +25,3 @@ plotLasso <- function(lambda,
   return(lasso_plot)
 }
 
-plotLasso(1.0)
-debug(plotLasso)
